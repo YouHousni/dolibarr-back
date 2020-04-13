babb224fa72bb182d7a421349123bf2e53242aa5
Othmane MANIAR <maniar.othmane@.com>
2020-04-13 17:32:21 +0200

    user dto

--- src/main/java/ma/cs/dolibar/User.java
+++ src/main/java/ma/cs/dolibar/User.java
@@ -1,4 +1,13 @@
 package ma.cs.dolibar;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
 }
